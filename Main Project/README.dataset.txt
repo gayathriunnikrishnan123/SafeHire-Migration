d6658eb6b25dc432c20daeb6c8a6857a0b92eb97
gayathriunnikrishnan123 <gayathriunnikrishnan000@gmail.com>
2024-04-16 17:26:05 +0530

    apr16

--- Main Project/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# aadhar card detection > 2023-06-14 1:24pm
-https://universe.roboflow.com/appinsource-v5mmj/aadhar-card-detection-nfk6r
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- Main Project/home/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CustomUser
-
-class PoliceOfficerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ('id', 'name')
